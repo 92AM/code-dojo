1ea6ad044fbddaf0ae41c395d92b2d6080940d7f
Arka Mitra <arka2k8@gmail.com>
2020-01-10 22:35:58 +0000

    Added a simple test to say Fizz

--- src/test/java/code/dojo/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/code/dojo/fizzbuzz/FizzBuzzTest.java
@@ -1,6 +1,15 @@
 package code.dojo.fizzbuzz;
 
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class FizzBuzzTest {
 
+	@Test
+	public void shouldSayFizz() {
+		assertThat("Fizz", IsEqual.equalTo("Fizz"));
+	}
 
 }
